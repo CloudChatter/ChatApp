943333c5610b698cf0cca6c1d42ed7719666f9e1
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-11-17 10:25:04 -0500

    set up HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const entry = ['./src/client/index.js'];
 
@@ -20,6 +21,9 @@ module.exports = {
     proxy: {
       '/api/': 'http://localhost:3000',
     },
+    hot: true,
+    inline: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
   },
   module: {
     rules: [
@@ -34,12 +38,19 @@ module.exports = {
         },
       },
       {
-        test: /\.s[ac]ss$/i,
+        // test: /\.s[ac]ss$/i,
+        test: /\.css/i,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader'],
+        // 'sass-loader'],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
