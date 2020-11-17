baa8aae52d2fbfa0ba538ee2ef732ab0ba273f50
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-11-17 10:25:24 -0500

    testing HMR

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,17 +1,26 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import './styles.css';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      number: 0,
+    };
   }
 
   render() {
     return (
       <div>
-      <h1>Hello Squirtle World!</h1>
-    </div>
-    )
+        <h1>Hello Squirtle World!</h1>
+        <p>Current value of number is {this.state.number}</p>
+        <button
+          onClick={() => this.setState({ number: this.state.number + 1 })}
+        >
+          Change State
+        </button>
+      </div>
+    );
   }
 }
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,26 @@
-import express from 'express'
-const app = express()
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/build', express.static(path.join(__dirname, '../../build')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../index.html'));
+  });
+
+  app.use(({ code, error }, req, res, next) => {
+    res.status(code).json({ error });
+  });
+}
+
+console.log(process.env.PG_URI);
+
+module.exports = app.listen(port, () =>
+  console.log(`Listening on port ${port}`)
+);
