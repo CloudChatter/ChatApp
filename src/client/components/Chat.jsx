7527c9615f50bf6aceb00fd07b8b0c5d940a8593
Andrew <andrew.lovato@gmail.com>
2020-11-17 12:22:33 -0500

    small changes to build out sockets

--- src/client/components/Chat.jsx
+++ src/client/components/Chat.jsx
@@ -1,51 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import io from 'socket.io-client'
 const socket = io();
 
 
 export const Chat = () => {
+  // access the currentUser contained within state
+  // dispatch the changes to the input: access the current state of those changes
+  // access current state of the input -> to send that text
+  // access the state of messages
+  const [value, updateValue] = useState('')
+
+
+  const [messages, updateMessages] = useState([])
+
   // get the state of the current username
   // array of messages
-  let messages = useSelector(state => state.messages)
+  // let messages = useSelector(state => state.messages)
   // a dynamic input string
-  let value = useSelector(state => state.inputText)
+  // let value = useSelector(state => state.inputText)
   const dispatch = useDispatch()
 
   socket.on('message', (data) => {
     console.log('connected client side!')
-    console.log(data)
-    dispatch({ type: 'addMessage', payload: { data } })
+    // dispatch({ type: 'addMessage', payload: { data } })
+    messages.updateMessages([messages])
+    console.log(messages)
+    console.log('leaving ')
   })
 
   function handleChange(e) {
-    const data = {
-      inputText: e.target.value
-    }
-    dispatch({ type: 'handleChange', payload: { data } })
+    // const data = {
+    //   inputText: e.target.value
+    // }
+    updateValue(e.target.value)
+    // dispatch({ type: 'handleChange', payload: data })
   }
 
   function handleSubmitChat(e) {
     console.log(e)
 
     e.preventDefault()
-    const data = {
+    const message = {
       text: value,
-      dateCreated: Date.now()
+      username: 'iLoveDogs',
+      createdAt: Date.now()
     }
-
-
-    socket.emit('message', data)
-    // dispatch({ type: 'addNewChat', payload: { data } })
+    socket.emit('message', message)
   }
 
   return (
     <div>
       <h3>Chat Room</h3>
       <ul className='messageList'>
-        {/* {messages.map((message) => {
+        {messages.map((message) => {
           return <li>{message.username} - {messsage.text}</li>
-        })} */}
+        })}
       </ul>
       <input value={value} onChange={handleChange} type="text" />
       <button onClick={handleSubmitChat}>Post!</button>
--- src/server/server.js
+++ src/server/server.js
@@ -6,26 +6,30 @@ const io = require('socket.io')(server);
 const path = require('path');
 
 io.on('connection', (socket) => {
-  console.log('socket.io is connected');
-
-  io.on('connection', (socket) => {
-    console.log('socket.io is connected on the server')
-    socket.on('message', data => {
-      console.log('message on the server', data)
-      io.emit('message', data)
-    })
+  console.log('socket.io is connected on the server')
+  socket.on('message', data => {
+    console.log('message on the server', data)
+    io.emit('message', data)
   })
+})
 
-  app.use('/build', express.static(path.join(__dirname, '../../build')));
 
-  // app.get('/build/bundle.js', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '../../build/bundle.js'));
-  // });
+// createa  button to save the current chat to DB (post reequest to db)
+// send along the username
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../index.html'));
-  });
+// DB: ILoveDogs = [{[{}{}{}],[{}{}{}{}]]
 
-  server.listen(3000, () => {
-    console.log('server listening at port 3000');
-  });
+
+app.use('/build', express.static(path.join(__dirname, '../../build')));
+
+app.get('/build/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build/bundle.js'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../index.html'));
+});
+
+server.listen(3000, () => {
+  console.log('server listening at port 3000');
+});
