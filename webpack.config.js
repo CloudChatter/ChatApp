4d45a46591f0a2ff1f781a24003636a96c3eb444
Andrew <andrew.lovato@gmail.com>
2020-11-17 10:17:39 -0500

    prelim setup for sockets

--- webpack.config.js
+++ webpack.config.js
@@ -22,15 +22,16 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [ '@babel/preset-env', '@babel/preset-react' ]
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       },
+
       // {
       //   test: /\.s[ac]ss$/i,
       //   use: [ 'style-loader', 'css-loader', 'sass-loader' ]
       // }
     ]
+
   },
-};    
-          
+};
