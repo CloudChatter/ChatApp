411efa1224b86a9bf873f8104e810852946e37df
Garrett James <garrettjames55@gmail.com>
2020-11-17 20:20:17 -0500

    started implmenting express server

---
+++ src/server/controllers/messageController.js
@@ -0,0 +1,43 @@
+const db = require('../controllers/messageController');
+
+const messageController = {};
+
+messageController.postMessage = (req, res, next) => {
+  const { message_id, message, created_at } = req.body;
+
+  const query = `
+  INSERT INTO Messages (message_id, message, created_at)
+  VALUES ($1, $2, $3, $4)
+`;
+  const values = [message_id, message, created_at];
+  db.query(query, values)
+    .then((data) => {
+      console.log(data);
+      return next();
+    })
+    .catch((err) => {
+      return next({
+        log: "Could not add message. Check query syntax.",
+        message: { error: err },
+      });
+    });
+};
+
+messageController.getMessages = (req, res, next) => {
+  const query = `SELECT * FROM Messages`;
+
+  db.query(query)
+    .then((data) => {
+      res.locals.messages = data.rows;
+      return next();
+    })
+    .catch((err) => {
+      return next({
+        log: "Could not get messages. Check query syntax.",
+        message: { error: err },
+      });
+    });
+};
+
+
+module.exports = messageController;
